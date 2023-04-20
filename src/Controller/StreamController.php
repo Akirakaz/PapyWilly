466fb05076cd81854ba36c95f92a684571965f23
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-04-20 02:13:55 +0200

    Add: stream controller

---
+++ src/Controller/StreamController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StreamController extends AbstractController
+{
+    #[Route('/stream', name: 'app_stream')]
+    public function index(): Response
+    {
+        return $this->render('public/stream/index.html.twig');
+    }
+}

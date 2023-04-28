39a2796b0238c89a3f34a5e6c2624d33b98ec8eb
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-04-28 13:53:49 +0200

    Add: url constraint

--- src/Form/SocialNetworkType.php
+++ src/Form/SocialNetworkType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Url;
 
 class SocialNetworkType extends AbstractType
 {
@@ -40,7 +41,12 @@ class SocialNetworkType extends AbstractType
                 'label' => "Url du service",
                 'attr' => [
                     'placeholder' => 'https://',
-                ]
+                ],
+                'constraints' => [
+                    new Url([
+                        'message' => "L'url n'est pas valide",
+                    ]),
+                ],
             ]);
     }
 

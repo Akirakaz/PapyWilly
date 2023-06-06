611968046e3a101094e86d9783ec9bddf14e29d4
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-06-06 03:06:19 +0200

    Update: setting recuperation method

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,18 +4,22 @@ namespace App\Controller;
 
 use App\Repository\HardwareRepository;
 use App\Repository\SettingsRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @throws NonUniqueResultException
+     */
     #[Route('/', name: 'app_home')]
     public function index(HardwareRepository $hardwareRepository, SettingsRepository $settingsRepository): Response
     {
         return $this->render('public/home/index.html.twig', [
             'hardwares' => $hardwareRepository->findAll(),
-            'settings' => $settingsRepository->findAll()[0],
+            'settings' => $settingsRepository->getSettings(),
         ]);
     }
 }

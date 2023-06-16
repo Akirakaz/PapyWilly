2fd338bf4b8c7191713e2ba3c3585651f843035d
Akirakaz <98087420+Akirakaz@users.noreply.github.com>
2023-06-16 05:46:05 +0200

    refactor: Stream Controller

--- src/Controller/StreamController.php
+++ src/Controller/StreamController.php
@@ -2,69 +2,48 @@
 
 namespace App\Controller;
 
-use App\Repository\SettingsRepository;
+use App\Service\TwitchAPIService;
+use App\Service\TwitchScheduleService;
+use DateInterval;
+use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use GuzzleHttp\Client;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class StreamController extends AbstractController
 {
     /**
+     * @throws Exception
      * @throws GuzzleException
      * @throws InvalidArgumentException
      */
     #[Route('/stream', name: 'app_stream')]
-    public function index(SettingsRepository $settingsRepository): Response
+    public function index(TwitchScheduleService $twitchSchedule, TwitchAPIService $twitchAPI): Response
     {
         $cache = new FilesystemAdapter();
-        $clientId = $this->getParameter('app.twitch_client_id');
-        $clientSecret = $this->getParameter('app.twitch_client_secret');
-        $channelId = $this->getParameter('app.twitch_channel_id');
+        $streamSchedule = $cache->getItem('stream_schedule');
 
-        $cacheKey = 'upcoming_streams';
-        $cache->deleteItem($cacheKey);
-
-        $cachedStreams = $cache->getItem($cacheKey);
-        if (!$cachedStreams->isHit()) {
-            $client = new Client();
-
-            $response = $client->post('https://id.twitch.tv/oauth2/token', [
-                'form_params' => [
-                    'client_id' => $clientId,
-                    'client_secret' => $clientSecret,
-                    'grant_type' => 'client_credentials',
-                ],
-            ]);
-
-            $data = json_decode($response->getBody(), true);
-            $accessToken = $data['access_token'];
-
-            $response = $client->get("https://api.twitch.tv/helix/schedule?broadcaster_id={$channelId}&first=6", [
-                'headers' => [
-                    'Client-ID' => $clientId,
-                    'Authorization' => "Bearer {$accessToken}",
-                ],
-            ]);
-
-            $data = json_decode($response->getBody(), true);
-            $upcomingStreams = $data['data']['segments'];
-
-            $cachedStreams->set($upcomingStreams);
-            $cache->save($cachedStreams);
-            dump('notcatched');
-        } else {
-            $upcomingStreams = $cachedStreams->get();
-            dump('catched');
+        if (!$streamSchedule->isHit()) {
+            $streamSchedule->set($twitchAPI->getSchedule());
+            $streamSchedule->expiresAfter(new DateInterval('PT1H'));
+            $cache->save($streamSchedule);
         }
 
+        $data = $streamSchedule->get();
+
+        $renderSchedule = $twitchSchedule->render($data);
 
         return $this->render('public/stream/index.html.twig', [
-            'upcoming_streams' => $upcomingStreams,
-            'settings' => $settingsRepository->findAll()[0],
+
+            'schedule'      => $renderSchedule['schedule'],
+            'scheduleWeek'  => [
+                'start' => $renderSchedule['scheduleWeek']['start'],
+                'end'   => $renderSchedule['scheduleWeek']['end']
+            ],
+            'twitchChannel' => $data['broadcaster_name'],
         ]);
     }
 }

993fd19b2a2a08eb771091b4932e85064e3d77b4
Guillaume SIONNEAU <guillaume.sionneau@gmail.com>
2023-06-19 20:31:50 +0200

    Add Youtube Service

---
+++ src/Service/YoutubeService.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Service;
+
+//use App\Service\CacheService;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class YoutubeService
+{
+    private $httpClient;
+    private array $parameters;
+//    private $cacheService;
+//    private $channelId;
+//    private $apiKey;
+
+    public function __construct(HttpClientInterface $httpClient, ParameterBagInterface $parameterBag)
+    {
+        $this->httpClient = $httpClient;
+        $this->parameters = [
+            'apiKey' => $parameterBag->get('app.youtube_api_key'),
+            'channelId' => $parameterBag->get('app.youtube_channel_id'),
+        ];
+//        $this->cacheService = $cacheService;
+//        $this->apiKey = $apiKey;
+//        $this->channelId= $channelId;
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function requestYoutube(int $limit = 6): array
+    {
+        $response = $this->httpClient->request('GET', 'https://www.googleapis.com/youtube/v3/search', [
+            'query' => [
+                'part' => 'snippet',
+                'channelId' => $this->parameters['channelId'],
+                'maxResults' => $limit,
+                'order' => 'date',
+                'type' => 'video',
+                'key' => $this->parameters['apiKey'],
+            ],
+        ]);
+
+        return $response->toArray();
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function getLatestVideos(int $limit = 6): array
+    {
+        $data = $this->requestYoutube($limit);
+
+        $videos = [];
+
+        foreach ($data['items'] as $item) {
+            $id = $item['id']['videoId'];
+            $videos[] = [
+                'id' => $id,
+                'title' => $item['snippet']['title'],
+                'description' => $item['snippet']['description'],
+                'thumbnail' => "https://i.ytimg.com/vi/$id/sddefault.jpg",
+                'publishedAt' => $item['snippet']['publishedAt'],
+            ];
+        }
+
+        return $videos;
+    }
+
+}
